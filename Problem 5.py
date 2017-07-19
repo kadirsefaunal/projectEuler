f66ec656b0a5e6469b2e5ffe3bf3091aa6633335
kadirsefaunal <kadirsefau@gmail.com>
2017-07-19 10:16:51 +0300

    Problem 5 çözümü tamamlandı.

--- Problem 5.py
+++ Problem 5.py
@@ -1,21 +1,45 @@
+import math
+
 def main():
-    asallar = [11, 13, 17, 19]
-    asalOlmayanlar = [12, 14, 15, 16, 18, 20]
+    liste = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     taban = 2520
 
-    #for value in asallar:
-    #    taban *= value
+    print(taban)
+
+    for value in liste:
+        if taban % value != 0:
+            if asalMi(value):
+                taban *= value
+            else:
+                asalBolenleri = asalBul(value)
+                for i in asalBolenleri:
+                    taban *= i
+                    if taban % value == 0:
+                        break
 
     print(taban)
 
-    for value in asallar:
-        if (taban % value != 0):
-            taban *= value
 
-    for value in asalOlmayanlar:
-        #kendisinden küçük asal çarpanlarla çarp
+def asalBul(deger):
+    asalBolenler = []
+    h = (deger ** 0.5) / 2
+    for value in range(2, round(deger)):
+        if deger % value == 0:
+            if asalMi(value):
+                asalBolenler += [value]
 
-    print(taban)
+    return asalBolenler
+
+def asalMi(sayi):
+    hedef = sayi / 2
+    durum = True
+    for j in range(2, round(hedef)):
+        durum = True
+        if sayi % j == 0:
+            durum = False
+            break
+
+    return durum
 
 if __name__ == "__main__":
     main()
