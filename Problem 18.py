9d615c960ebcfcae15d6ccc27502c54d95c67849
kadirsefaunal <kadirsefau@gmail.com>
2017-07-21 10:59:16 +0300

    Problem 18 çözümü tamamlandı.

--- Problem 18.py
+++ Problem 18.py
@@ -2,8 +2,8 @@ def main():
     dosya = open("Problem 18.txt")
     satirlar = dosya.readlines()
     sayilar = []
-    grup = []
     toplam = 0
+    indis = 0
     for value in satirlar:
         satir = value.split()
         satirSayisi = len(satir) + 1
@@ -11,22 +11,16 @@ def main():
         for sayi in satir:
             sayilar.append(int(sayi))
 
-        indis = satir.index(str(max(sayilar)))
-        grup.append(sayilar[indis])
-        if (indis - 1 >= 0):
-            grup.append(sayilar[indis - 1])
-        if (indis + 1 < satirSayisi):
-            grup.append(sayilar[indis + 1])
+        grupMax = sayilar[indis]
+        if len(sayilar) > 1:
+            if sayilar[indis + 1] > grupMax:
+                grupMax = sayilar[indis + 1]
+                indis = indis + 1
 
-        toplam += max(grup)
+        toplam += grupMax
         sayilar = []
-        grup = []
-
-        print(max(sayilar), satir.index(str(max(sayilar))), satirSayisi)
 
     print(toplam)
-
-
 
 
 if __name__ == '__main__':
